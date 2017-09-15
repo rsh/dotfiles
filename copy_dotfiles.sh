10079e4ce35ae343a2b0b38e7404e510bdd9efe2
Rayhan Hasan <rayhan.h@gmail.com>
2017-09-14 18:19:37 -0700

    Update copy_dotfiles.sh

--- copy_dotfiles.sh
+++ copy_dotfiles.sh
@@ -4,3 +4,4 @@
 
 curl https://raw.githubusercontent.com/rsh/dotfiles/master/.tmux.conf > ~/.tmux.conf
 curl https://raw.githubusercontent.com/rsh/dotfiles/master/.vimrc > ~/.vimrc
+curl https://raw.githubusercontent.com/rsh/dotfiles/master/.bash_profile > ~/.bash_profile
