05436fb0580cfb6803e476fe72df108693fc726a
Rayhan Hasan <rayhan.h@gmail.com>
2020-08-14 14:00:12 -0700

    Include VS Code settings in copy_dotfiles.sh

--- copy_dotfiles.sh
+++ copy_dotfiles.sh
@@ -5,3 +5,7 @@
 curl https://raw.githubusercontent.com/rsh/dotfiles/master/.tmux.conf > ~/.tmux.conf
 curl https://raw.githubusercontent.com/rsh/dotfiles/master/.vimrc > ~/.vimrc
 curl https://raw.githubusercontent.com/rsh/dotfiles/master/.bash_profile > ~/.bash_profile
+curl https://raw.githubusercontent.com/rsh/dotfiles/master/vscode-settings.json > ~/settings.json
+
+echo "VS Code settings have been copied to ~/settings.json"
+echo "Please copy them to the appropriate place for this platform."
